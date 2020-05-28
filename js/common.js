f4cfb4e9cf5efa5593318868e74fadc80344bd9c
Denis Filipovich <philippovichdenis@gmail.com>
2020-05-29 00:12:27 +0300

    optimized for ie

--- js/common.js
+++ js/common.js
@@ -1,77 +1,96 @@
-
 var slideIndex = 1;
-var objects = ["./images/yogurt1.png","./images/yogurt2.png","./images/yogurt3.png"];
-var texts = ["./images/page2__yogurt-text-1.png","./images/page2__yogurt-text-2.png","./images/page2__yogurt-text-3.png"];
-var zzz = [["./images/object2.png", "./images/object1.png"],["./images/object6.png","./images/object5.png"],["./images/object4.png", "./images/object3.png"]];
-var screens = document.querySelectorAll('.screen');
+var objects = [
+  "./images/yogurt1.png",
+  "./images/yogurt2.png",
+  "./images/yogurt3.png",
+];
+var texts = [
+  "./images/page2__yogurt-text-1.png",
+  "./images/page2__yogurt-text-2.png",
+  "./images/page2__yogurt-text-3.png",
+];
+var zzz = [
+  ["./images/object2.png", "./images/object1.png"],
+  ["./images/object6.png", "./images/object5.png"],
+  ["./images/object4.png", "./images/object3.png"],
+];
+var screens = document.querySelectorAll(".screen");
 
 function plusSlides(n, cls) {
-    showSlides(slideIndex += n, cls);
+  showSlides((slideIndex += n), cls);
 }
 
 function showSlides(n, cls) {
-    var i;
-    var slides = document.querySelectorAll('.screen__object-container');
+  var i;
+  var slides = document.querySelectorAll(".screen__object-container");
 
-    if (n > slides.length) {
-        slideIndex = 1
-    }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
 
-    if (n < 1) {
-        slideIndex = slides.length
-    }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('active');
-        slides[i].classList.remove('fwd');
-        slides[i].classList.remove('bck');
-    }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    slides[i].classList.remove("fwd");
+    slides[i].classList.remove("bck");
+  }
 
-    slides[slideIndex - 1].classList.add('active');
-    slides[slideIndex - 1].classList.add(cls);
+  slides[slideIndex - 1].classList.add("active");
+  slides[slideIndex - 1].classList.add(cls);
 }
 
 function setImages(variant) {
-    var screen2Text = document.querySelector('.screen-2 .screen__text-1 img');
-    var screen2Img = document.querySelector('.screen-2 .screen__object img');
-    var screen4Img = document.querySelector('.screen-4 .screen__object img');
-    var ingridients = document.querySelectorAll('.screen-4 .screen__ingrideent img');
+  var screen2Text = document.querySelector(".screen-2 .screen__text-1 img");
+  var screen2Img = document.querySelector(".screen-2 .screen__object img");
+  var screen4Img = document.querySelector(".screen-4 .screen__object img");
+  var ingridients = document.querySelectorAll(
+    ".screen-4 .screen__ingrideent img"
+  );
+  screen2Text.setAttribute("src", texts[variant]);
+  screen2Img.setAttribute("src", objects[variant]);
+  screen4Img.setAttribute("src", objects[variant]);
+  console.log(zzz[variant]);
+  //   ingridients.forEach(function (el, index) {
+  //     el.setAttribute('src', zzz[variant][index]);
+  //   });
 
-    screen2Text.setAttribute('src', texts[variant]);
-    screen2Img.setAttribute('src', objects[variant]);
-    screen4Img.setAttribute('src', objects[variant]);
-    console.log(zzz[variant]);
-    ingridients.forEach((el,index) => {
-        el.setAttribute('src', zzz[variant][index]);
-    })
+  for (var i = 0, len = ingridients.length; i < len; i++) {
+    ingridients[i].setAttribute("src", zzz[variant][i]);
+  }
 }
 
 function setObject() {
-     var selectedObject = document.querySelector('.screen__object-container.active');
+  var selectedObject = document.querySelector(
+    ".screen__object-container.active"
+  );
+  var container = document.querySelector(".container");
+  var chose = selectedObject.getAttribute("data-object");
+  container.setAttribute("data-selected", chose);
+  setImages(chose);
 
-     var container = document.querySelector('.container');
-     var chose = selectedObject.getAttribute('data-object');
-     container.setAttribute('data-selected', chose);
-
-     setImages(chose);
-
-     screens.forEach((el) => {
-         el.classList.remove('active');
-     });
-
-     screens[1].classList.add('active');
+  for (var i = 0, len = screens.length; i < len; i++) {
+    screens[i].classList.remove("active");
+  }
+  //   screens.forEach(function (el) {
+  //     el.classList.remove('active');
+  //   });
+  screens[1].classList.add("active");
 }
 
 function startGame() {
-    screens.forEach((el) => {
-        el.classList.remove('active');
-    });
-
-    screens[3].classList.add('active');
+//   screens.forEach(function (el) {
+//     el.classList.remove("active");
+//   });
+  for (var i = 0, len = screens.length; i < len; i++) {
+    screens[i].classList.remove("active");
+  }
+  screens[3].classList.add("active");
 }
 
-window.addEventListener('load', function() {
-    var screen1 = this.document.querySelector('.screen.screen-1');
-    screen1.classList.add('active');
-})
-
+window.addEventListener("load", function () {
+  var screen1 = this.document.querySelector(".screen.screen-1");
+  screen1.classList.add("active");
+});
--- js/game.js
+++ js/game.js
@@ -1,15 +1,15 @@
 var globalType = null;
-
 var el = 0;
 var counter = 0;
 var progressElement = document.querySelector(".progress");
 var screen2 = document.querySelector(".screen.screen-2");
 var screen3 = document.querySelector(".screen.screen-3");
 var screen4 = document.querySelector(".screen.screen-4");
-
-let fruits = {
+var fruits = {
   cup: null,
-  apple: { w: 70 },
+  apple: {
+    w: 70
+  },
   zerno: null,
   malina: null,
   grecha: null,
@@ -19,28 +19,27 @@ let fruits = {
   width: 240,
   height: 400,
   isClicked: false,
-  init: function () {
+  init: function init() {
     this.canvas = document.getElementById("canvas");
     this.ctx = document.getElementById("canvas").getContext("2d");
     this.canvas.width = this.width;
     this.canvas.height = this.height;
     this.setEvents();
   },
-  setEvents() {
-    window.addEventListener("mousedown", (e) => {
-      this.isClicked = true;
-    });
+  setEvents: function setEvents() {
+    var _this = this;
 
-    window.addEventListener("mouseup", (e) => {
-      this.isClicked = false;
+    window.addEventListener("mousedown", function (e) {
+      _this.isClicked = true;
     });
-
-    window.addEventListener("mousemove", (e) => {
-      if (this.isClicked) {
-        this.cup.move(e.pageX);
+    window.addEventListener("mouseup", function (e) {
+      _this.isClicked = false;
+    });
+    window.addEventListener("mousemove", function (e) {
+      if (_this.isClicked) {
+        _this.cup.move(e.pageX);
       }
     });
-
     setInterval(function () {
       var el = getRandomInt(1, 7);
       var obj = null;
@@ -48,12 +47,12 @@ let fruits = {
       var maxSpeed = getRandomInt(100, 200);
       var graviry = 0.10;
 
-      if(counter > 5) {
-          graviry = 0.14
-      } else if(counter > 10) {
-        graviry = 0.20
-      } else if(counter > 15) {
-          graviry = 0.25
+      if (counter > 5) {
+        graviry = 0.14;
+      } else if (counter > 10) {
+        graviry = 0.20;
+      } else if (counter > 15) {
+        graviry = 0.25;
       }
 
       if (el == 1) {
@@ -76,49 +75,35 @@ let fruits = {
         type = 1;
       }
 
-      fruits.objects.push(
-        new Apple(
-          getRandomInt(0, fruits.width - fruits.apple.w),
-          -200,
-          obj,
-          maxSpeed,
-          type,
-          graviry
-        )
-      );
+      fruits.objects.push(new Apple(getRandomInt(0, fruits.width - fruits.apple.w), -200, obj, maxSpeed, type, graviry));
     }, 400);
   },
-  preload(callback) {
+  preload: function preload(callback) {
     this.cupI = new Image();
-    if(globalType == 0) {
+
+    if (globalType == 0) {
       this.cupI.src = "../images/yogurt1.png";
-    } else if(globalType == 1) {
+    } else if (globalType == 1) {
       this.cupI.src = "../images/yogurt2.png";
-    } else if(globalType == 2) {
+    } else if (globalType == 2) {
       this.cupI.src = "../images/yogurt3.png";
     }
 
     this.appleI = new Image();
     this.appleI.src = "../images/object1.png";
-
     this.zernoI = new Image();
     this.zernoI.src = "../images/object2.png";
-
     this.malinaI = new Image();
     this.malinaI.src = "../images/object3.png";
-
     this.grechaI = new Image();
     this.grechaI.src = "../images/object4.png";
-
     this.klukvaI = new Image();
     this.klukvaI.src = "../images/object5.png";
-
     this.cherryI = new Image();
     this.cherryI.src = "../images/object6.png";
-
     callback();
   },
-  render() {
+  render: function render() {
     this.ctx.clearRect(0, 0, this.width, this.height);
 
     for (i in this.objects) {
@@ -127,33 +112,31 @@ let fruits = {
       this.objects[i].count();
       this.objects[i].draw();
     }
-    this.ctx.drawImage(
-      this.cupI,
-      this.cup.x,
-      this.cup.y,
-      this.cup.w,
-      this.cup.h
-    );
-  },
 
-  update() {
-    //console.log(this.objects);
+    this.ctx.drawImage(this.cupI, this.cup.x, this.cup.y, this.cup.w, this.cup.h);
   },
-  run() {
-    window.requestAnimationFrame(() => {
-      this.update();
-      this.render();
-      this.run();
+  update: function update() {
+  },
+  run: function run() {
+    var _this2 = this;
+
+    window.requestAnimationFrame(function () {
+      _this2.update();
+
+      _this2.render();
+
+      _this2.run();
     });
   },
-  start: function () {
+  start: function start() {
+    var _this3 = this;
+
     this.init();
-    this.preload(() => {
-      this.run();
+    this.preload(function () {
+      _this3.run();
     });
-  },
+  }
 };
-
 fruits.cup = {
   w: 106,
   h: 125,
@@ -161,7 +144,7 @@ fruits.cup = {
   y: 330,
   dx: 0,
   dy: 0,
-  move(x) {
+  move: function move(x) {
     if (x <= fruits.width - this.w / 2 && x > 0 + this.w / 2) {
       this.x = x - this.w / 2;
     } else if (x > fruits.width - this.w / 2) {
@@ -169,52 +152,35 @@ fruits.cup = {
     } else {
       this.x = 0;
     }
-  },
+  }
 };
 
-var Apple = function (x, y, src, spd, type, dd) {
-  (this.x = x), (this.y = y), (this.dy = 0);
-  (this.max = spd),
-    (this.dd = dd),
-    (this.src = src),
-    (this.img = new Image()),
-    (this.img.src = src),
-    (this.type = type),
-    (this.isCount = false);
+var Apple = function Apple(x, y, src, spd, type, dd) {
+  this.x = x, this.y = y, this.dy = 0;
+  this.max = spd, this.dd = dd, this.src = src, this.img = new Image(), this.img.src = src, this.type = type, this.isCount = false;
 };
 
 Apple.prototype = {
-  move: function () {
+  move: function move() {
     this.y += this.dy;
   },
-  grav: function () {
+  grav: function grav() {
     this.dy += this.dy <= this.max ? this.dd : 0;
   },
-  draw: function () {
+  draw: function draw() {
     fruits.ctx.drawImage(this.img, this.x, this.y);
   },
-  count: function () {
-    if (
-      this.y +20 >= fruits.cup.y &&
-      this.x <= fruits.cup.w + fruits.cup.x &&
-      this.x  >= fruits.cup.x &&
-      !this.isCount &&
-      this.type == globalType
-    ) {
+  count: function count() {
+    if (this.y + 20 >= fruits.cup.y && this.x <= fruits.cup.w + fruits.cup.x && this.x >= fruits.cup.x && !this.isCount && this.type == globalType) {
       this.isCount = true;
       this.y = 1500;
       counter++;
-      console.log(counter);
     }
-    if (
-      this.y + 100 > fruits.cup.y &&
-      this.x + 80 > fruits.cup.w &&
-      this.x < fruits.cup.x &&
-      !this.isCount
-    ) {
+
+    if (this.y + 100 > fruits.cup.y && this.x + 80 > fruits.cup.w && this.x < fruits.cup.x && !this.isCount) {
       this.isCount = true;
     }
-  },
+  }
 };
 
 function getRandomInt(min, max) {
@@ -241,10 +207,7 @@ setInterval(function () {
 }, 100);
 
 function startGame() {
-  globalType = document
-    .querySelector(".container")
-    .getAttribute("data-selected");
-
+  globalType = document.querySelector(".container").getAttribute("data-selected");
   setTimeout(function () {
     screen2.classList.remove("active");
     screen3.classList.add("active");
